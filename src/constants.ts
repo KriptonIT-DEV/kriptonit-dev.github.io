a3cbd87bad714c779d0809da4cf1b84670d2a673
layn <layn@github.com>
2026-07-21 17:20:54 -0500

    add base envs

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const BASE_API_URL = 'https://kservice.konnect-360.pe'
+export const BASE_API_WIDGET = 'https://devkapi.konnect-360.pe'
+export const AUTH_REQUEST_TOKEN =
+  '**Requisito:** Primero debes [obtener un token de acceso](/reference/authentication).'
